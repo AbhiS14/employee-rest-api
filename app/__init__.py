264cf54d817ddef047e3bb198f01204e1ee0bf8e
AbhiS14 <abhi998677@gmail.com>
2026-02-27 10:21:49 +0530

    Docker container working and health endpoint fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from app.api import register_blueprints
 
+from flask import Flask
+from app.api import register_blueprints
+
 
 def create_app(config_name="development"):
     app = Flask(__name__)
